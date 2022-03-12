bcb81acb31c7a3b3a2d3ff5ee8a0ff044c54511b
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-12 20:51:21 -0300

    booklist css created

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 function BookList() {
    return (
-      <section>
+      <section className="booklist">
          <Book />
          <Book />
          <Book />
@@ -15,7 +16,7 @@ function BookList() {
 
 const Book = () => {
    return (
-      <article>
+      <article className="book">
          <Image />
          <Title />
          <Author />
