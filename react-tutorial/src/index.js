e204097c5b0a9fd3d3bd3a3482f3583b2de222e4
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-12 19:56:35 -0300

    first component

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,1 +1,8 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+
+function FirstComponent() {
+   return <h1>Hello, this is my firts React compsssonent</h1>;
+}
+
+ReactDOM.render(<FirstComponent />, document.getElementById("root"));
