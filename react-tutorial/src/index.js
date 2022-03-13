f98d96471326d72e647f781eb93313499d475f67
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-12 21:08:06 -0300

    use of javascript inside JSX
    and some Author CSS mods

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const img =
+   "https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg";
+const title = "Verity";
+const author = "Colleen Hoover";
+
 function BookList() {
    return (
       <section className="booklist">
@@ -17,24 +22,10 @@ function BookList() {
 const Book = () => {
    return (
       <article className="book">
-         <Image />
-         <Title />
-         <Author />
+         <img src={img} alt="Book Cover" />
+         <h1>{title}</h1>
+         <h4>{author}</h4>
       </article>
    );
 };
-
-const Image = () => {
-   return (
-      <img
-         src="https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg"
-         alt="Book Cover"
-      />
-   );
-};
-
-const Title = () => <h1>Verity</h1>;
-
-const Author = () => <h4>Colleen Hoover</h4>;
-
 ReactDOM.render(<BookList />, document.getElementById("root"));
