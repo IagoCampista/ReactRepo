a7c13392edddafdd522a3b7e15ef69bddcfa24d4
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-12 22:09:23 -0300

    KeyProp and spread operator

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,30 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-const firstBook = {
-   img: "https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg",
-   title: "Verity",
-   author: "Colleen Hoover",
-};
-const secondBook = {
-   img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
-   title: "Atomic Habits",
-   author: "James Clear",
-};
+const books = [
+   {
+      id: 1,
+      img: "https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg",
+      title: "Verity",
+      author: "Colleen Hoover",
+   },
+   {
+      id: 2,
+      img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
+      title: "Atomic Habits",
+      author: "James Clear",
+   },
+   {
+      id: 3,
+      img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
+      title: "I Love You to the Moon and Back",
+      author: "Amelia Hepworth",
+   },
+];
 
 function BookList() {
    return (
       <section className="booklist">
-         <Book
-            img={firstBook.img}
-            title={firstBook.title}
-            author={firstBook.author}
-         />
-         <Book
-            img={secondBook.img}
-            title={secondBook.title}
-            author={secondBook.author}
-         />
+         {books.map((book) => {
+            return <Book key={book.id} {...book}></Book>;
+         })}
       </section>
    );
 }
