6b8938e3c8cc1c73d9ea1eba2395b501fd5e30ad
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-12 21:36:23 -0300

    How props work

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,30 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-const img =
-   "https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg";
-const title = "Verity";
-const author = "Colleen Hoover";
+const firstBook = {
+   img: "https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg",
+   title: "Verity",
+   author: "Colleen Hoover",
+};
+const secondBook = {
+   img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
+   title: "Atomic Habits",
+   author: "James Clear",
+};
 
 function BookList() {
    return (
       <section className="booklist">
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-         <Book />
+         <Book
+            img={firstBook.img}
+            title={firstBook.title}
+            author={firstBook.author}
+         />
+         <Book
+            img={secondBook.img}
+            title={secondBook.title}
+            author={secondBook.author}
+         />
       </section>
    );
 }
 
-const Book = () => {
+const Book = (props) => {
    return (
       <article className="book">
-         <img src={img} alt="Book Cover" />
-         <h1>{title}</h1>
-         <h4>{author}</h4>
+         <img src={props.img} alt="Book Cover" />
+         <h1>{props.title}</h1>
+         <h4>{props.author}</h4>
       </article>
    );
 };
