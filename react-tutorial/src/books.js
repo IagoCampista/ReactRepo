265c20556d956312fc036b4ff47bcfe19d4612cb
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2022-03-14 21:15:45 -0300

    How Export works

---
+++ react-tutorial/src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+   {
+      id: 1,
+      img: "https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg",
+      title: "Verity",
+      author: "Colleen Hoover",
+   },
+   {
+      id: 2,
+      img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
+      title: "Atomic Habits",
+      author: "James Clear",
+   },
+   {
+      id: 3,
+      img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
+      title: "I Love You to the Moon and Back",
+      author: "Amelia Hepworth",
+   },
+];
--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,26 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-const books = [
-   {
-      id: 1,
-      img: "https://images-na.ssl-images-amazon.com/images/I/61+nBGbGW7L._AC_UL604_SR604,400_.jpg",
-      title: "Verity",
-      author: "Colleen Hoover",
-   },
-   {
-      id: 2,
-      img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL604_SR604,400_.jpg",
-      title: "Atomic Habits",
-      author: "James Clear",
-   },
-   {
-      id: 3,
-      img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
-      title: "I Love You to the Moon and Back",
-      author: "Amelia Hepworth",
-   },
-];
+import { books } from "./books";
 
 function BookList() {
    return (
@@ -33,11 +14,15 @@ function BookList() {
 }
 
 const Book = ({ img, title, author }) => {
+   const showAuthor = (author) => {
+      alert(author);
+   };
    return (
       <article className="book">
          <img src={img} alt="Book Cover" />
          <h1>{title}</h1>
          <h4>{author}</h4>
+         <button onClick={() => showAuthor(author)}>Show Author</button>
       </article>
    );
 };
